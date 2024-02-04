fae57b2e658c772e7a6ebdca1b6f8a218f3970b5
Anderson Alves <contatodeanderson@gmail.com>
2024-02-04 20:23:05 -0300

    add entities Genre, Movie e Review

--- src/main/java/com/devsuperior/movieflix/entities/Genre.java
+++ src/main/java/com/devsuperior/movieflix/entities/Genre.java
@@ -1,11 +1,12 @@
 package com.devsuperior.movieflix.entities;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,24 +14,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "tb_review")
+@Table(name = "tb_genre")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Data
-public class Review {
+public class Genre {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  private String text;
+  private String name;
 
-  @ManyToOne
-  @JoinColumn(name = "user_id")
-  private User user;
-
-  @ManyToOne
-  @JoinColumn(name = "movie_id")
-  private Movie movie;
+  @OneToMany(mappedBy = "genre")
+  private List<Movie> movies = new ArrayList<>();
 }
--- src/main/java/com/devsuperior/movieflix/entities/Movie.java
+++ src/main/java/com/devsuperior/movieflix/entities/Movie.java
@@ -1,11 +1,14 @@
 package com.devsuperior.movieflix.entities;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,24 +16,31 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "tb_review")
+@Table(name = "tb_movie")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Data
-public class Review {
+public class Movie {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  private String text;
+  private String title;
+
+  private String subTitle;
+
+  private Integer year;
+
+  private String imgUrl;
+
+  private String synopsis;
 
   @ManyToOne
-  @JoinColumn(name = "user_id")
-  private User user;
+  @JoinColumn(name = "genre_id")
+  private Genre genre;
 
-  @ManyToOne
-  @JoinColumn(name = "movie_id")
-  private Movie movie;
+  @OneToMany(mappedBy = "movie")
+  private List<Review> reviews = new ArrayList<>();
 }
